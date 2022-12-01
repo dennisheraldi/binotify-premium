cb1bf9735c49d14c196c11cf69878f0d90a202e3
Fachry Dennis Heraldi <3688+dennisheraldi@users.noreply.gitlab.informatika.org>
2022-12-02 04:11:09 +0700

    style: tidy up code song

--- src/pages/Song.jsx
+++ src/pages/Song.jsx
@@ -7,15 +7,6 @@ import BasicCard from "../components/common/BasicCard/BasicCard";
 import SongTable from "../components/SongTable/SongTable";
 import axios from "axios";
 
-const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0LCJlbWFpbCI6InNpbmdlcjFAZ21haWwuY29tIiwidXNlcm5hbWUiOiJzaW5nZXIxIiwibmFtZSI6InNpbmdlciAxIiwiaXNBZG1pbiI6ZmFsc2UsImlhdCI6MTY2OTgzNDgyMX0.ohKhj20QspLcJ8GCTjDdWwFMXE2Fwc_nfeMZMeqo3Uo";
-
-const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
-    timeout: 1000,
-    headers: { Authorization: `Bearer ${token}` },
-});
-
 export function Song() {
     const title = "Kelola Lagu";
 
